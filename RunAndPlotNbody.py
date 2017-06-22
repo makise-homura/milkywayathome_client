184bbb0a8ce008e7c1f5ec943b3cb87e28ada6d0
Clayton Rayment <simharry3@gmail.com>
2017-06-22 10:30:23 +0000

    Temporary fix to command line arguments to satisfy sid's changes

--- RunAndPlotNbody.py
+++ RunAndPlotNbody.py
@@ -17,13 +17,13 @@ os.system('rm CPUACCTEST.out')
 print "RUNNING CPU SYSTEM:"
 os.system("cmake -DNBODY_OPENCL=OFF -DDOUBLEPREC=ON -DNBODY_OPENMP=ON -DCMAKE_BUILD_TYPE=RELEASE")
 os.system('make -j 9')
-executeString = './bin/milkyway_nbody -f nbody/sample_workunits/for_developers.lua -o CPUBRUTE.out -z CPU.hist -b -i 1 1 .2 1 12 1' # >> CPUACCTEST.out'
+executeString = './bin/milkyway_nbody -f nbody/sample_workunits/for_developers.lua -o CPUBRUTE.out -z CPU.hist -b -i 1 1 .2 .2 12 .2' # >> CPUACCTEST.out'
 os.system(executeString)
 
 print "RUNNING GPU SYSTEM:"
 os.system("cmake -DNBODY_OPENCL=ON -DDOUBLEPREC=ON -DCMAKE_BUILD_TYPE=RELEASE ")
 os.system('make -j 9')
-executeString = './bin/milkyway_nbody -f nbody/sample_workunits/for_developers.lua -o GPUBRUTE.out -z GPU.hist -b -i 1 1 .2 1 12 1' # >> GPUACCTEST.out'
+executeString = './bin/milkyway_nbody -f nbody/sample_workunits/for_developers.lua -o GPUBRUTE.out -z GPU.hist -b -i 1 1 .2 .2 12 .2' # >> GPUACCTEST.out'
 os.system(executeString)
 
 
